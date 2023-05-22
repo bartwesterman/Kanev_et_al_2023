991cf85ba7df0123b452dc1eea7df52ea6315dd2
Bart Westerman, PhD <47714729+bartwesterman@users.noreply.github.com>
2023-05-21 20:46:20 -0500

    added prep_sparse_set.py

--- scripts/prep_sparse_set.py
+++ scripts/prep_sparse_set.py
@@ -1,11 +1,23 @@
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 root = os.path.dirname(os.getcwd())
-D = pd.read_csv(root + "/data/data.csv", 
-                names=["epxeriment", "uniprot", "kinase", "group", "inchi_key", "value", "smiles", "mw"])
+D = pd.read_csv(root + "/data/data_ml.csv")
+print(D.shape)
 
-train, test = train_test_split(D, stratify=D["kinase"], test_size=0.15)
+train, test = train_test_split(D, stratify=D["kinase"], test_size=0.3, random_state=42)
+
+cpds_train_set = train["inchi_key"].values
+
+to_move = test[test['inchi_key'].isin(cpds_train_set)]
+
+print(test.shape[0], to_move.shape[0], test.shape[0]-to_move.shape[0])
+
+train = pd.concat([train, to_move])
+test = test.drop(to_move.index)
+print(train.shape)
+print(test.shape)
 
 test.to_csv(root + "/data/sparse_test.csv", index=False)
 train.to_csv(root + "/data/sparse_train.csv", index=False) 
